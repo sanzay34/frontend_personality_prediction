86ca6a34ee31a19f65ae73e852716530f0581d46
sanzay34 <sanzay321@gmail.com>
2025-01-27 17:50:21 +0545

    footer  finished

--- src/components/autoscroll.jsx
+++ src/components/autoscroll.jsx
@@ -13,7 +13,7 @@ const Autoscroll = () => {
 				<img
 					src={value}
 					alt="Value"
-					className="w-[200px] h-[200px] mt-[90px]"
+					className="w-[200px] h-[200px] mt-[90px] rounded-full"
 				/>
 			</div>
 			<div className="text-white text-[60px] mt-5 text-center">Values</div>
@@ -29,7 +29,7 @@ const Autoscroll = () => {
 			className="bg-customGray-50 w-[650px] h-[700px] flex flex-col items-center justify-center rounded-lg"
 		>
 			<div>
-				<img src={vision} alt="Vision" className="w-[200px] h-[200px]" />
+				<img src={vision} alt="Vision" className="w-[200px] h-[200px] rounded-full" />
 			</div>
 			<div className="text-white text-[60px] mt-5 text-center">Vision</div>
 			<p className="text-white text-[25px] mt-5 text-center">
@@ -41,7 +41,7 @@ const Autoscroll = () => {
 			className="bg-customGray-50 w-[650px] h-[700px] flex flex-col items-center justify-center rounded-2xl"
 		>
 			<div>
-				<img src={mission} alt="Mission" className="w-[200px] h-[200px]" />
+				<img src={mission} alt="Mission" className="w-[200px] h-[200px] rounded-full" />
 			</div>
 			<div className="text-white text-[60px] mt-5 text-center">Mission</div>
 			<p className="text-white text-[25px] mt-5 text-center">
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
-
-const Footer
- = () => {
-  return (
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFacebook,
+	faTwitter,
+	faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+const Footer = () => {
+	return (
 		<div className="bg-customGray-50 w-[1600px] h-[500px] ml-[100px] rounded-3xl mb-[50px] relative px-[42px] py-[58px] text-white">
 			<div className="px-4 text-white text-center border border-white w-[350px] h-[70px] ">
 				<div className="text-5xl font-serif">Aura Traits</div>
-				<div className="text-sm font-light tracking-[8px] ">
+				<div className="text-sm font-light tracking-[8px]">
 					Personality Predictor
 				</div>
 			</div>
 			<div className="mt-8 text-2xl">
 				Discover and embrace your unique personality traits.
-          </div>
-          <div className='flex justify-between w-[40%] absolute bottom-[85px] text-xl'>
-              <div>Home</div>
-              <div>About Us</div>
-              <div>Explore</div>
-              <div>Check your Personality Traits</div>
-          </div>
-			<hr className="absolute bottom-[75px] left-0 w-full  border-t-2 border-white " />
+			</div>
+			<div className="flex justify-between w-[40%] absolute bottom-[85px] text-xl">
+				<div>Home</div>
+				<div>About Us</div>
+				<div>Explore</div>
+				<div>Check your Personality Traits</div>
+			</div>
+			<div className="absolute bottom-[85px] text-xl left-[1325px]">auratraits@gmail.com</div>
+			<div className="absolute bottom-[120px] left-[1300px] flex space-x-9 flex-row">
+				{/* Facebook Icon */}
+				<FontAwesomeIcon
+					icon={faFacebook}
+					size="2x"
+					className="text-white text-[40px]  cursor-pointer"
+				/>
+				<FontAwesomeIcon
+					icon={faTwitter}
+					size="2x"
+					className="text-white text-[40px]  cursor-pointer"
+				/>
+				<FontAwesomeIcon
+					icon={faInstagram}
+					size="2x"
+					className="text-white text-[40px]  cursor-pointer"
+				/>
+				<FontAwesomeIcon
+					icon={faEnvelope}
+					size="2x"
+					className="text-white text-[40px]  cursor-pointer"
+				/>
+			</div>
+			<hr className="absolute bottom-[75px] left-0 w-full border-t-2 border-white" />
+			<div className="absolute bottom-[30px] text-lg">
+				<span className="border border-white rounded-full px-[4px]">C</span>
+				<span> 2024 Aura Traits. All Rights Reserved</span>
+			</div>
 		</div>
 	);
-}
+};
 
-export default Footer
+export default Footer;
